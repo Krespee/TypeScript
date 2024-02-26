0eb5e131cfdf342fe1430d77d8b14e729cd23d70
nayn <naynzahir@gmail.com>
2024-02-26 11:56:09 -0300

    Interface & type

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 "use strict";
-// Types personalizados
-let programador = {
+//interface funciona igual al type
+// type Programador = {
+//     nombre:string,
+//     tecnologias:string[],
+//     estudia?: boolean
+// }
+let dev = {
     nombre: "nayn",
     tecnologias: ["react", "node"],
     estudia: true
 };
 // corroborar que el valor que le agregamos a la propiedad simpre sea el mismo que tipamos en el Type personalizado, otra opcion es agregar con "| "tipo de dato""
-let programador2 = {
-    nombre: "fede",
-    tecnologias: ["cobol"],
+let dev2 = {
+    name: "fede",
+    tech: ["cobol"],
     // estudia: null
+    lastname: "hla",
+    recibido: false
 };
-console.log(programador2);
+function enviarCV(programador) {
+    console.log(`Este Curriculum es de ${programador.nombre}`);
+}
+enviarCV(dev);
--- script.ts
+++ script.ts
@@ -1,21 +1,38 @@
-// Types personalizados
-
-type Programador = {
+// Interface
+// no hace falta el =
+interface Programador {
     nombre:string,
     tecnologias:string[],
     estudia?: boolean
 }
 
-let programador: Programador = {
+//interface funciona igual al type
+
+// type Programador = {
+//     nombre:string,
+//     tecnologias:string[],
+//     estudia?: boolean
+// }
+
+
+let dev: Programador = {
     nombre: "nayn",
     tecnologias: ["react", "node"],
     estudia: true
 };
 
 // corroborar que el valor que le agregamos a la propiedad simpre sea el mismo que tipamos en el Type personalizado, otra opcion es agregar con "| "tipo de dato""
-let programador2: Programador ={
-    nombre: "fede",
-    tecnologias: ["cobol"],
+
+let dev2 ={
+    name: "fede",
+    tech: ["cobol"],
     // estudia: null
+    lastname: "hla",
+    recibido: false
 }
-console.log(programador2)
+
+function enviarCV(programador: Programador){
+    console.log(`Este Curriculum es de ${programador.nombre}`)
+}
+
+enviarCV(dev)
