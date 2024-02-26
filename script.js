ad9e5b04145b9fc316a253f0943afdeb23484806
nayn <naynzahir@gmail.com>
2024-02-26 10:19:50 -0300

    Datos Primitivos

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("Mode Observer");
+// datos tipo booleanos
+// let estudiasteJavascript: boolean = true;
+// if (estudiasteJavascript) {
+//     console.log("Podes seguir el curso")
+// }else{
+//     console.log("Primero debes estudiar")
+// }
+//datos tipo number
+let interMiami = 11;
+let fcDallas = 11;
+let messi = 1;
+let juegaMessi = true;
+let palabras = "Holaaa";
+function jugar(equipo1, equipo2, juegaMessi) {
+    let motivo = "";
+    if (juegaMessi) {
+        equipo1 += messi;
+        motivo = "porque juega mesi";
+    }
+    if (equipo1 > equipo2)
+        console.log(`Gana inter ${motivo}`);
+    if (equipo1 == equipo2)
+        console.log("Empatan");
+    if (equipo1 < equipo2)
+        console.log("Gana Dallas");
+}
+jugar(interMiami, fcDallas, juegaMessi);
--- script.ts
+++ script.ts
@@ -1,1 +1,35 @@
-console.log("Mode Observer")
+// datos tipo booleanos
+
+// let estudiasteJavascript: boolean = true;
+
+// if (estudiasteJavascript) {
+//     console.log("Podes seguir el curso")
+// }else{
+//     console.log("Primero debes estudiar")
+// }
+
+//datos tipo number
+
+let interMiami: number  | null | undefined= 11
+let fcDallas: number = 11
+
+let messi: number = 1
+let juegaMessi: boolean= true
+
+let palabras: string = "Holaaa"
+
+
+function jugar(equipo1:number, equipo2: number, juegaMessi: boolean):void{
+    let motivo: string = ""
+
+    if (juegaMessi) {
+    equipo1 += messi
+    motivo = "porque juega mesi"
+    }
+    if (equipo1 > equipo2) console.log(`Gana inter ${motivo}`)
+    if (equipo1 == equipo2) console.log("Empatan")
+    if (equipo1 < equipo2) console.log("Gana Dallas")
+}
+
+jugar(interMiami, fcDallas, juegaMessi)
+
