bfbaa777179bd484341c2226ca07fe5d9b38ee57
nayn <naynzahir@gmail.com>
2024-02-26 14:34:37 -0300

    Class & POO

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 "use strict";
-//interface funciona igual al type
-// type Programador = {
-//     nombre:string,
-//     tecnologias:string[],
-//     estudia?: boolean
-// }
-let dev = {
-    nombre: "nayn",
-    tecnologias: ["react", "node"],
-    estudia: true
-};
-// corroborar que el valor que le agregamos a la propiedad simpre sea el mismo que tipamos en el Type personalizado, otra opcion es agregar con "| "tipo de dato""
-let dev2 = {
-    name: "fede",
-    tech: ["cobol"],
-    // estudia: null
-    lastname: "hla",
-    recibido: false
-};
-function enviarCV(programador) {
-    console.log(`Este Curriculum es de ${programador.nombre}`);
+// Class & POO
+class Pelicula {
+    proyectar() {
+        console.log(`${this.nombre} está siendo proyectada`);
+    }
+    constructor(nombre, protagonistas, actores) {
+        this.nombre = nombre,
+            this.protagonistas = protagonistas,
+            this.actores = actores;
+    }
 }
-enviarCV(dev);
+const pelicula = new Pelicula("barbie", ["barbie", "ken"], ["margon", "rian"]);
+const pelicula2 = new Pelicula("oppenheimer", ["oppenheimer", "straus"], ["cillian", "robert"]);
+pelicula2.proyectar();
--- script.ts
+++ script.ts
@@ -1,38 +1,22 @@
-// Interface
-// no hace falta el =
-interface Programador {
-    nombre:string,
-    tecnologias:string[],
-    estudia?: boolean
-}
+// Class & POO
 
-//interface funciona igual al type
+    class Pelicula{
+        nombre?: string;
+        protagonistas?:string[];
+        actores?: string[];
 
-// type Programador = {
-//     nombre:string,
-//     tecnologias:string[],
-//     estudia?: boolean
-// }
+        proyectar():void{
+            console.log(`${this.nombre} está siendo proyectada`)
+        }
+
+        constructor(nombre: string, protagonistas:string[], actores:string[]){
+            this.nombre = nombre,
+            this.protagonistas = protagonistas,
+            this.actores = actores
+        }
+    }
 
 
-let dev: Programador = {
-    nombre: "nayn",
-    tecnologias: ["react", "node"],
-    estudia: true
-};
-
-// corroborar que el valor que le agregamos a la propiedad simpre sea el mismo que tipamos en el Type personalizado, otra opcion es agregar con "| "tipo de dato""
-
-let dev2 ={
-    name: "fede",
-    tech: ["cobol"],
-    // estudia: null
-    lastname: "hla",
-    recibido: false
-}
-
-function enviarCV(programador: Programador){
-    console.log(`Este Curriculum es de ${programador.nombre}`)
-}
-
-enviarCV(dev)
+const pelicula = new Pelicula ("barbie", ["barbie", "ken"], ["margon", "rian"])
+const pelicula2 = new Pelicula ("oppenheimer", ["oppenheimer", "straus"], ["cillian", "robert"])
+pelicula2.proyectar()
